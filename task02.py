35e3499bba1875024b3a54a94ccb15760ad9a096
ali mekhridinov <mehriddinovali19@gmail.com>
2026-03-24 15:50:59 +0500

    task02 done

---
+++ task02.py
@@ -0,0 +1,9 @@
+password = "ali1234"
+user_input = ""
+
+while user_input != password:
+    user_input = input("Parolingizni kiriting: ")
+    if user_input != password:
+        print("Xato, parol notog'ri! ")
+
+print("xo'sh kelinsiz")
