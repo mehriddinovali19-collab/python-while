0f6886b4d2bd04e697fb94143187a9360957e317
ali mekhridinov <mehriddinovali19@gmail.com>
2026-03-25 09:23:00 +0500

    task05 done

---
+++ task05.py
@@ -0,0 +1,11 @@
+count = 0
+
+while True:
+    text = input("Matn kiriting: ")
+
+    if text == "stop":
+        break 
+    else:
+        count += 1
+
+print("Siz", count, "marta matn kiritdingiz.")
